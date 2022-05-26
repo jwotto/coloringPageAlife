27a83aeecbf73b9d22cf47bad5d62520d76b177a
jwotto <janwotto@hotmail.com>
2022-05-26 12:33:17 +0200

    Update test.py

--- test.py
+++ test.py
@@ -2,18 +2,17 @@
 import cv2 as cv
 import numpy as np
 import imutils
-  
+
 path = 'Photos/cat.png'
 
 image = cv.imread(path)
-
 
 window_name = 'image'
   
 
 #cv.imshow(window_name, image)
 
-rotated = imutils.rotate(image, 0)
+rotated = imutils.rotate(image, 5)
 cv.imshow("Rotated by 180 Degrees", rotated)
 #cv.getRotationMatrix2D(0,0,40)
 
