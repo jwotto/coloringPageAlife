b0a4858f43f2aabcf61516845e8f5c3365980817
jwotto <janwotto@hotmail.com>
2022-06-04 13:58:01 +0200

    Update ArUcoTest.py

--- ArUcoTest.py
+++ ArUcoTest.py
@@ -3,10 +3,19 @@ import cv2 as cv
 import cv2.aruco as aruco
 import numpy as np
 import os
+import time
 
+camera = 0
+cameraWidth = 1920
+cameraHeight = 1080
 
 def main():
-    video = cv.VideoCapture(1)
+    video = cv.VideoCapture(camera)
+    video.set(3, cameraWidth)
+    video.set(4, cameraHeight)
+    time.sleep(2)
+
+
     while True:
         ret, frame = video.read()
         cv.imshow("cam", frame)
@@ -16,3 +25,6 @@ def main():
 
     video.release()
     cv.destroyAllWindows()
+
+
+main()
