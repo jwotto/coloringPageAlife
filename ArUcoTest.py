37d776d4571457c289f342bcf95917d2b65f25fd
jwotto <janwotto@hotmail.com>
2022-06-05 13:20:33 +0200

    new aruco marker tracking

--- ArUcoTest.py
+++ ArUcoTest.py
@@ -1,4 +1,5 @@
 import cv2 as cv
+from pandas import array
 import cv2.aruco as aruco
 import numpy as np
 import time
@@ -7,12 +8,9 @@ import time
 camera = 1
 cameraWidth = 1280
 cameraHeight = 720
-frameRate = 60
+frameRate = 20
 
-pointLeftTop = 0
-pointRightTop = 0
-pointRightBottom = 0
-pointLeftBottom = 0
+cornersPage = [(0,0),(0,0),(0,0),(0,0)] # [leftTop,rightTop,rightBottom,leftBottom]
 
 
 
@@ -51,11 +49,17 @@ def main():
                 cv.putText(frame, str(ids[i]), int_corners[i][0][(int((ids[i]+2) % 4))],
                            cv.FONT_HERSHEY_SIMPLEX, 1, 255, 3)
 
+                
+                #collect all corners
+                int_id = int((ids[i])%4)
+                cornersPage[int_id] = int_corners[i][0][(int((ids[i]+2) % 4))]
+                
+                
                 #draw the colering page area when all aruco codes are found  this code needs to be edited
                 if totalAruco == 4:
-                    cv.line(frame, int_corners[i][0][(int((ids[i]+2) % 4))], int_corners[(
-                        i+1) % 4][0][(int((ids[(i+1) % 4]+2) % 4))], (0, 255, 255), 5)
+                    cv.line(frame, cornersPage[i], cornersPage[(i+1)%4], (0, 255, 255), 5)
 
+            
 
             # what coloring page
             if ids[0] <= 3:
@@ -64,6 +68,9 @@ def main():
             if ids[0] >= 4 and ids[[0]] < 7:
                 cv.putText(frame, "Raam", (10, 50),
                            cv.FONT_HERSHEY_SIMPLEX, 2, 255, 6)
+                
+        print(cornersPage)
+       
 
         cv.imshow("cam", frame)
         if cv.waitKey(frameRate) == ord('x'):
