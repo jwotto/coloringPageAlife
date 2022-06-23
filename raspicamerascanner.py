b8e0b5da30e03af928b577a25d331e034806cfde
jwotto <janwotto@hotmail.com>
2022-06-23 21:12:03 +0200

    .

--- raspicamerascanner.py
+++ raspicamerascanner.py
@@ -15,14 +15,15 @@ GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 #ledsetup
 PIXEL_PIN = board.D10
-NUM_PIXELS = 195
+NUM_PIXELS_LEDRING = 60
+NUM_PIXELS_BOX = 135
+NUM_PIXELS = NUM_PIXELS_LEDRING + NUM_PIXELS_BOX
 ORDER = neopixel.GRB
 
 #camera setup
-CAMERA = 0
 CAMERAWIDTH = int(1280)
 CAMERAHEIGHT = int(720)
-FRAME_RATE = 30
+FRAME_RATE = 40
 
 #colering pages lis
 COLLERING_PAGE_NAME = ['Gebouw','Raam',]
@@ -53,7 +54,10 @@ def uniqueIdCornerCheck(idList):
         return False
 
 
+
 def main():
+
+    
     # set video capture
     camera = PiCamera()
     camera.resolution = (CAMERAWIDTH, CAMERAHEIGHT)
@@ -63,15 +67,12 @@ def main():
     raw_capture = PiRGBArray(camera, size=(CAMERAWIDTH, CAMERAHEIGHT))
  
     # Wait a certain number of seconds to allow the camera time to warmup
-    time.sleep(0.1)
+    time.sleep(2)
     
     # Load Aruco detectorx
     parameters = cv.aruco.DetectorParameters_create()
     aruco_dict = cv.aruco.Dictionary_get(cv.aruco.DICT_5X5_50)
 
-    #wait time dor camera to startup
-    time.sleep(2)
-    
     #set corners for collering page
     corners_page = [(0,0),(0,0),(0,0),(0,0)] # [leftTop,rightTop,rightBottom,leftBottom]
     converted_points = np.float32(
@@ -82,6 +83,34 @@ def main():
 
     #bolean for making a picture
     picture_ready = False
+
+    button_pressed =False
+
+
+    def pixels_scanned_correct():
+        for i in range(NUM_PIXELS_LEDRING):
+            pixels[i] = (0, 255, 0)
+        for j in range(NUM_PIXELS_BOX):
+            pixels[NUM_PIXELS_LEDRING+j] = (0 ,255 , 0)
+        pixels.show()
+
+    def pixels_scanned_uncorrect():
+        for i in range(NUM_PIXELS_LEDRING):
+            pixels[i] = (0,0,255)
+        for j in range(NUM_PIXELS_BOX):
+            pixels[NUM_PIXELS_LEDRING+j] = (0,0,255)
+        pixels.show()
+
+    def pixels_white():
+        for i in range(NUM_PIXELS_LEDRING):
+            pixels[i] = (0, 0, 0)
+        for j in range(NUM_PIXELS_BOX):
+            pixels[NUM_PIXELS_LEDRING+j] = (255 ,255 , 255)
+        pixels.show()
+
+
+    
+
 
     #draw loop
     for frame in camera.capture_continuous(raw_capture, format="bgr", use_video_port=True):
@@ -130,49 +159,57 @@ def main():
                     # make a picture from the lighted image
                     cv.imshow("Warped perspective", img_output)
                     cv.imwrite('/home/pi/share/scan.jpg', img_output)
-                    picture_ready = False
-                
 
+                    picture_ready = False
+                        
+
+                        
+
+
+                    
+    
 
             #only draw the colering page area when all aruco codes are found and there all the same group id 
             if total_aruco == 4 and uniqueIdCornerCheck(id_list) == True and picture_ready == False:
-                #SETLED GREEN WHEN DETECTED
-                pixels.fill((0, 255, 0))
-                pixels.show()
+               
+                pixels_scanned_correct()
+
                 #draw lines around the scanned picture
-                for j in range(4):
+                for j in range(2):
                     cv.line(frame, corners_page[j], corners_page[(j+1)%4], (255, 0, 255), 2)
                 
                 button_value = GPIO.input(17) 
-                if (button_value == True):
-                    pixels.fill((255, 255, 255))
-                    pixels.show()
+                if (button_value == True or button_pressed == True):
+                    pixels_white()
+                    time.sleep(1)
                     picture_ready = True
-                    #een frame wachten voordat hij de fot maakt?
+                    button_pressed = False
+                    
 
             #when no 4 aruco codes are found   
             else:
-                pixels.fill((0, 125, 255))
-                pixels.show()
+                pixels_scanned_uncorrect()
 
             # shows name scanned colering page
             cv.putText(frame,COLLERING_PAGE_NAME[groupId(ids[0])], (10, 50),
                         cv.FONT_HERSHEY_SIMPLEX, 2, 255, 6)
 
         else:
-            
-            pixels.fill((0, 125, 255))
-            pixels.show()
+            pixels_scanned_uncorrect()
 
         cv.imshow("cam", frame)
+        
         # Clear the stream in preparation for the next frame
         raw_capture.truncate(0)
 
         key = cv.waitKey(1) & 0xFF
         if key == ord("x"):
             break
+        if key == ord("p"):
+            button_pressed = True
+            
 
-    cv.destroyAllWindows()
+    #cv.destroyAllWindows()
     pixels.fill((0, 0, 0))
     pixels.show()
 
